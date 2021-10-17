c3f67cfa1a4b5d397a95690d29cdb05f176e4153
amirulasri <amirulasrix@gmail.com>
2021-10-17 14:48:09 +0800

    Update

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow() {
     const win = new BrowserWindow({
         width: 1500,
-        height: 800
+        height: 800,
+        icon: __dirname + '/wanaicon.ico'
     })
     win.setMenu(null)
     win.loadFile('index.html')
